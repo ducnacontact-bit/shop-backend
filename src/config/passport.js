73fedf27df318737a5a0e05f5eeba50bd1621d66
Ducs <ducna.contact@gmail.com>
2026-03-19 03:35:40 +0700

    fix passport env

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,73 +1,148 @@
-const passport = require("passport")
-const GoogleStrategy = require("passport-google-oauth20").Strategy
-const FacebookStrategy = require("passport-facebook").Strategy
-const { PrismaClient } = require("@prisma/client")
+// const passport = require("passport")
+// const GoogleStrategy = require("passport-google-oauth20").Strategy
+// const FacebookStrategy = require("passport-facebook").Strategy
+// const { PrismaClient } = require("@prisma/client")
 
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
+
+// // GOOGLE LOGIN
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: "/api/auth/google/callback"
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+
+//       let user = await prisma.user.findFirst({
+//         where: {
+//           provider: "google",
+//           providerId: profile.id
+//         }
+//       })
+
+//       if (!user) {
+//         user = await prisma.user.create({
+//           data: {
+//             email: profile.emails?.[0]?.value,
+//             provider: "google",
+//             providerId: profile.id
+//           }
+//         })
+//       }
+
+//       done(null, user)
+//     }
+//   )
+// )
+
+// // FACEBOOK LOGIN
+// passport.use(
+//   new FacebookStrategy(
+//     {
+//       clientID: process.env.FB_APP_ID,
+//       clientSecret: process.env.FB_APP_SECRET,
+//       callbackURL: "/api/auth/facebook/callback",
+//       profileFields: ["id","emails","name"]
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+
+//       let user = await prisma.user.findFirst({
+//         where: {
+//           provider: "facebook",
+//           providerId: profile.id
+//         }
+//       })
+
+//       if (!user) {
+//         user = await prisma.user.create({
+//           data: {
+//             email: profile.emails?.[0]?.value,
+//             provider: "facebook",
+//             providerId: profile.id
+//           }
+//         })
+//       }
+
+//       done(null, user)
+//     }
+//   )
+// )
+
+// module.exports = passport
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 // GOOGLE LOGIN
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback"
-    },
-    async (accessToken, refreshToken, profile, done) => {
-
-      let user = await prisma.user.findFirst({
-        where: {
-          provider: "google",
-          providerId: profile.id
-        }
-      })
-
-      if (!user) {
-        user = await prisma.user.create({
-          data: {
-            email: profile.emails?.[0]?.value,
+if (process.env.GOOGLE_CLIENT_ID) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "/api/auth/google/callback",
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        let user = await prisma.user.findFirst({
+          where: {
             provider: "google",
-            providerId: profile.id
-          }
-        })
-      }
+            providerId: profile.id,
+          },
+        });
 
-      done(null, user)
-    }
-  )
-)
+        if (!user) {
+          user = await prisma.user.create({
+            data: {
+              email: profile.emails?.[0]?.value,
+              provider: "google",
+              providerId: profile.id,
+            },
+          });
+        }
+
+        done(null, user);
+      },
+    ),
+  );
+}
 
 // FACEBOOK LOGIN
-passport.use(
-  new FacebookStrategy(
-    {
-      clientID: process.env.FB_APP_ID,
-      clientSecret: process.env.FB_APP_SECRET,
-      callbackURL: "/api/auth/facebook/callback",
-      profileFields: ["id","emails","name"]
-    },
-    async (accessToken, refreshToken, profile, done) => {
-
-      let user = await prisma.user.findFirst({
-        where: {
-          provider: "facebook",
-          providerId: profile.id
-        }
-      })
-
-      if (!user) {
-        user = await prisma.user.create({
-          data: {
-            email: profile.emails?.[0]?.value,
+if (process.env.FB_APP_ID) {
+  passport.use(
+    new FacebookStrategy(
+      {
+        clientID: process.env.FB_APP_ID,
+        clientSecret: process.env.FB_APP_SECRET,
+        callbackURL: "/api/auth/facebook/callback",
+        profileFields: ["id", "emails", "name"],
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        let user = await prisma.user.findFirst({
+          where: {
             provider: "facebook",
-            providerId: profile.id
-          }
-        })
-      }
+            providerId: profile.id,
+          },
+        });
 
-      done(null, user)
-    }
-  )
-)
+        if (!user) {
+          user = await prisma.user.create({
+            data: {
+              email: profile.emails?.[0]?.value,
+              provider: "facebook",
+              providerId: profile.id,
+            },
+          });
+        }
 
-module.exports = passport
+        done(null, user);
+      },
+    ),
+  );
+}
+
+module.exports = passport;
